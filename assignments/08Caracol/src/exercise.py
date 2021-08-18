e5a410cdae03d35938d9150f9f9537e46886d735
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-08-18 10:08:00 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,5 +2,17 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+minutos = float(input("Introduce los minutos que correrá el caracol:"))
+#5.7mm/s
+
+
+distancia = (5.7/10)*60
+
+print("La distancia que hará en",minutos, "minutos es:",distancia, "cm")
+
+
+
 if __name__ == '__main__':
     main()
+
+    
