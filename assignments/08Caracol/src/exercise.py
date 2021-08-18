bf1b63bb1032e9dd0aacdded93ce3ebb1d6bf248
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-08-18 18:17:32 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,3 +1,4 @@
+import os
 def main():
     #escribe tu código abajo de esta línea
     pass
@@ -12,9 +13,9 @@ distancia = (5.7/10)*(60*minutos)
 print("Centímentros recorridos:",distancia)
 
 
-
 if __name__ == '__main__':
     main()
+os.system("pause")
 
 
 
