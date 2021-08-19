e80b664164d07c16fb4a36efcd2264ebbcda324d
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-08-19 00:36:57 -0500

    Update exercise.py
    Me gustaría comentar que para la realización de este programa lo fui dividiendo en secciones más pequeñas y consultando en internet, aprendí a hacer funciones, ciclos, condiciones y  descubrí nuevas funciones integradas, espero que esté bien, traté de optimizar.

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,7 +1,28 @@
+#DigitosPares
 def main():
     #escribe tu código abajo de esta línea
     pass
 
+
+lista = []
+def digitospares(número):
+    for i in número:
+        a = i
+        mod_calc = float(a) % 2
+
+        if mod_calc % 2 == 0:
+            lista.append(int(a))
+       
+   
+
+número = input("Dame un número:")
+digitospares(número)
+
+par = len(lista)
+if par == 0:
+    print("no hay ningún número par")
+else:    
+    print("El número de digitos pares es:",par)
 
 if __name__ == '__main__':
     main()
