aeb5a4a8060353c0c72bbaa38cad6621f1002d57
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-08-18 22:35:11 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,29 @@
+#Publicación en una editorial
+import math
 def main():
     #escribe tu código abajo de esta línea
     pass
+
+palabras=int(input("Dame el número de palabras:"))
+ 
+
+páginas = 1
+
+if palabras > 475:
+    
+    redondeado = math.ceil(palabras / 475)
+    páginas = redondeado * páginas
+    páginas = páginas * 60
+    páginas = páginas * .9
+    print("El costo de la publicación es:",páginas)
+
+
+else:
+    
+    páginas = 60
+    páginas = páginas * .9
+
+    print("El costo de la publicación es:",páginas)
 
 
 if __name__ == '__main__':
