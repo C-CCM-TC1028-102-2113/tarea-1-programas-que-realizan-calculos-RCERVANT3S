a328d4349041db57dc8d3c2e3b679b3411088bcb
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-08-18 22:46:37 -0500

    Update exercise.py

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,14 @@
+#LevaduraParaPizza
 def main():
     #escribe tu código abajo de esta línea
     pass
+
+gramos = float(input("Dame la harina en gramos:"))
+#50 gramos de levadura = 1000 gramos de harina
+levadura = gramos / 20
+
+ 
+print("Necesitas estos gramos de levadura:",levadura)
 
 
 if __name__ == '__main__':
