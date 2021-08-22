7b12d6ea1dc9fe29cbde0e2e5324810af142e033
RCERVANT3S <89056561+RCERVANT3S@users.noreply.github.com>
2021-08-22 12:54:56 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    minutos = float(input("Dame los minutos:"))
+    pass
+
+minutos = float(input("Dame los minutos: "))
 
 
 
@@ -8,11 +10,9 @@ def main():
 distancia = (5.7/10)*(60*minutos)
 
 print("Centímentros recorridos:",distancia)
-    pass
-
-
 
 
 
 if __name__ == '__main__':
     main()
+
